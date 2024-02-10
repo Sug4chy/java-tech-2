48d411bc3247a9e53b3694a58305657f94b6f50b
Степан <sb.9226981433@gmail.com>
2024-02-10 22:28:47 +0500

    made Post a data class

--- src/main/kotlin/com/example/demo/model/Post.kt
+++ src/main/kotlin/com/example/demo/model/Post.kt
@@ -3,17 +3,16 @@ package com.example.demo.model
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
-import java.io.Serializable
 import java.util.*
 
 @Entity
-class Post(
+data class Post(
     @Id
     @GeneratedValue
     val id: Long?,
     val text: String,
     val creationDate: Date,
     var likes: Int = 0,
-): Serializable {
+){
     constructor() : this(0, "", Date())
 }
